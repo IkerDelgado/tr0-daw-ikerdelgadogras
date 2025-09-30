78c0dd845e99f702fcedec22300fb55ee0eb42a9
IkerDelgado <a23ikedelgra@inspedralbes.cat>
2025-09-30 10:52:17 +0200

    implementación de .env

--- api/db.php
+++ api/db.php
@@ -1,7 +1,7 @@
 <?php
-$host = 'host.docker.internal'; // o la IP pública del servidor MySQL
-$user = 'root';
-$pass = ''; // pon aquí la contraseña que te hayan dado o la que hayas puesto
+$host = 'localhost'; // o la IP pública del servidor MySQL
+$user = 'a23ikedelgra_a23ikedelgra';
+$pass = 'InsPedralbes2025'; // pon aquí la contraseña que te hayan dado o la que hayas puesto
 $dbname = 'a23ikedelgra_proyecte-tr0';
 
 $conn = new mysqli($host, $user, $pass, $dbname);
